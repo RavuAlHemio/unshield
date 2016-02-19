a6571ae049737d932af58e47f31fbe0536c3d7a8
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-02-19 17:21:11 +0100

    Add custom getopt implementation for Windows.

--- src/unshield.c
+++ src/unshield.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include "wingetopt.h"
 #include "../lib/libunshield.h"
 #ifdef HAVE_CONFIG_H
 #include "lib/unshield_config.h"
